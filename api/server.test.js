c39d5c1b2b5bc16d43c4ae934164d73845ca2115
DarkedgeTH <darkedgeth@gmail.com>
2022-04-28 18:50:04 -0400

    finished POST login tests

--- api/server.test.js
+++ api/server.test.js
@@ -29,21 +29,26 @@ describe('[POST] /register', () => {
     result = await Users.findById(1)
     expect(result.username).toBe('Beth')
   })
-  test('accurate response for failed register', async () => {
-    const res = await request(server).post('/api/auth/register').send({
-      username: '',
-      password: '1234'
-    })
-    expect(res.body).toMatchObject({ message: 'username and password required' })
+  test('accurate response for failed register with duplicate username', async () => {
+    const result = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'Beth', password: '1234' })
+    expect(result.body.message).toMatch(/username taken/i)
   })
 })
 
 describe('[POST] /login', () => {
-  test('accurate response for successful login', () => {
-
+  test('accurate response for successful login', async () => {
+    let result = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'Beth', password: '1234' })
+    expect(result.body.message).toMatch(/welcome, Beth/i)
   })
-  test('accurate response for failed login', () => {
-    
+  test('accurate response for failed login with wrong password', async () => {
+    let result = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'Beth', password: 'abcd' })
+    expect(result.body.message).toMatch(/invalid credentials/i)
   })
 })
 
