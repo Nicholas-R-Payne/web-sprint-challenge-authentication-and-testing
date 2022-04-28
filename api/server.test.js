170f4e4990382f18fd3d9bbbe53c9157b0daa9e4
DarkedgeTH <darkedgeth@gmail.com>
2022-04-28 18:32:12 -0400

    finished POST register tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,18 @@
 const request = require('supertest')
 const db = require('../data/dbConfig')
+const server = require('./server')
+const Users = require('./users/users-model')
 
 // Write your tests here
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
@@ -11,11 +22,19 @@ test('make sure environment is set correctly', () => {
 })
 
 describe('[POST] /register', () => {
-  test('accurate response for successful register', () => {
-
+  test('user in database after successful register', async () => {
+    let result = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'Beth', password: '1234' })
+    result = await Users.findById(1)
+    expect(result.username).toBe('Beth')
   })
-  test('accurate response for failed register', () => {
-    
+  test('accurate response for failed register', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: '',
+      password: '1234'
+    })
+    expect(res.body).toMatchObject({ message: 'username and password required' })
   })
 })
 
